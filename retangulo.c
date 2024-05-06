28a8dea7cf674eb6a6c60a6d037fe4072f1b1ef8
VitorFrango <51138494+VitorFrango@users.noreply.github.com>
2024-05-06 21:41:36 +0100

    Retangulo c e h

---
+++ retangulo.c
@@ -0,0 +1,33 @@
+//
+// Created by Vitor Frango on 06/05/2024.
+//
+
+#include "retangulo.h"
+#include <stdio.h>
+
+static Retangulo retangulos[MAX_RETANGULOS];  // Array de retângulos
+static int retangulo_conta = 0; // Contador de retângulos
+
+
+void criar_retangulo(int x, int y, int largura, int altura) {
+    if (retangulo_conta >= MAX_RETANGULOS) {
+        printf("Erro: número máximo de retângulos atingido\n");
+        return;
+    }
+    // Cria um novo retângulo com as coordenadas e dimensões fornecidas
+    retangulos[retangulo_conta++] = (Retangulo) {x, y, largura, altura};
+    aplicar_gravidade();
+}
+
+void mover_retangulo(int x, int y, int desvio, bool correto) {
+    // Move o retângulo especificado para a nova posição
+    for (int i = 0; i < retangulo_conta; i++) {
+        if (retangulos[i].x <= x && x < retangulos[i].x + retangulos[i].largura &&
+           retangulos[i].y - retangulos[i].altura < y && y <= retangulos[i].y) {
+            retangulos[i].x += correto ? desvio : -desvio;
+            aplicar_gravidade();
+            return;
+            }
+        }
+    printf("Erro: nenhum retângulo encontrado nessas coordenadas. \n");
+}
---
+++ retangulo.h
@@ -0,0 +1,24 @@
+//
+// Created by Vitor Frango on 06/05/2024.
+//
+
+#ifndef EFOLIOB2324_RETANGULO_H
+#define EFOLIOB2324_RETANGULO_H
+
+#define MAX_RETANGULOS 100
+#define MAX_LARGURA 80
+#define MAX_ALTURA 25
+
+typedef struct Retangulo {
+    int x;
+    int y;
+    int largura;
+    int altura;
+} Retangulo;
+
+void criar_retangulo(int x , int y, int largura, int altura);
+void mover_retangulo(int x, int y, desvio, bool correto);
+void desenhar_grelha(void);
+void aplicar_gravidade(void);
+
+#endif //EFOLIOB2324_RETANGULO_H
