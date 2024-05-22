c9d303bbb52b0282f93b670a054b387af528f667
VitorFrango <51138494+VitorFrango@users.noreply.github.com>
2024-05-23 00:05:52 +0100

    copia 2023Global

---
+++ cmake-build-debug/CMakeFiles/AF3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vitorfrango/Library/CloudStorage/OneDrive-UniversidadeAberta/1 Ano/Laboratorio de Programação/EfolioB2324/AF3/grelha.c" "CMakeFiles/AF3.dir/grelha.c.o" "gcc" "CMakeFiles/AF3.dir/grelha.c.o.d"
+  "/Users/vitorfrango/Library/CloudStorage/OneDrive-UniversidadeAberta/1 Ano/Laboratorio de Programação/EfolioB2324/AF3/main.c" "CMakeFiles/AF3.dir/main.c.o" "gcc" "CMakeFiles/AF3.dir/main.c.o.d"
+  "/Users/vitorfrango/Library/CloudStorage/OneDrive-UniversidadeAberta/1 Ano/Laboratorio de Programação/EfolioB2324/AF3/menu.c" "CMakeFiles/AF3.dir/menu.c.o" "gcc" "CMakeFiles/AF3.dir/menu.c.o.d"
+  "/Users/vitorfrango/Library/CloudStorage/OneDrive-UniversidadeAberta/1 Ano/Laboratorio de Programação/EfolioB2324/AF3/retangulo.c" "CMakeFiles/AF3.dir/retangulo.c.o" "gcc" "CMakeFiles/AF3.dir/retangulo.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AF3.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AF3.
+# This may be replaced when dependencies are built.
